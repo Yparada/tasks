15d718246e596182f75d2e4a1cb19a550534572e
Yparada <yparadat@gmail.com>
2022-02-17 21:35:51 -0500

    Find all, find by id, update and delete project project methods added

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,5 +1,30 @@
 import Project from "../models/Project";
 
+export async function getProjects(req, res) {
+    try {
+        const projects = await Project.findAll();
+        res.json({
+            message: 'Query success',
+            data: projects
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getProjectById(req, res) {
+    const { id } = req.params;
+    const project = await Project.findOne({
+        where: {
+            id
+        }
+    });
+    res.json({
+        message: 'Query success',
+        data: project
+    });
+}
+
 export async function createProject(req, res) {
     const { name, priority, description, deliverydate } = req.body;
     try {
@@ -23,6 +48,53 @@ export async function createProject(req, res) {
         res.status(500).json({
             message: 'Something goes wrong',
             data: {}
+        });
+    }
+}
+
+export async function updateProject(req, res){
+    const { id } = req.params;
+    const { name, priority, description, deliverydate } = req.body;
+
+    const projects = await Project.findAll({
+        attributes: ['id','name','priority','description','deliverydate'],
+        where: {
+            id
+        }
+    });
+
+    if(projects.length > 0){
+        projects.forEach(async project => {
+            await project.update({
+                name,
+                priority,
+                description,
+                deliverydate
+            });
         })
+    }
+
+    return res.json({
+        message: 'Project update success',
+        data: projects
+    });
+}
+
+export async function deleteProject(req, res) {
+    try {
+        const { id } = req.params;
+        const deleteRowCount = await Project.destroy({
+            where: {
+                id
+            }
+        });
+        res.json({
+            message: 'Project deleted success',
+            data: {
+                count: deleteRowCount
+            }
+        });
+    } catch (error) {
+        console.log(error);
     }
 }
--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 const router = Router();
 
-import { createProject } from '../controllers/project.controller';
+import { createProject, getProjects, getProjectById, deleteProject, updateProject } from '../controllers/project.controller';
 
 // /api/projects/
 router.post('/', createProject);
+router.get('/', getProjects);
+router.get('/:id', getProjectById);
+router.put('/:id', updateProject);
+router.delete('/:id', deleteProject);
 
 export default router;
