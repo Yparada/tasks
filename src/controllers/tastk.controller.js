45d2b058fc26f5cc31944c062a2c9125cbcba376
Yparada <yparadat@gmail.com>
2022-02-17 22:19:26 -0500

    Task id corrected -  create, read and delete task funtions added

---
+++ src/controllers/tastk.controller.js
@@ -0,0 +1,61 @@
+import Task from '../models/Tasks';
+
+export async function createTask(req, res) {
+    try {
+        const { name, done, projectId } = req.body;
+        const newTask = await Task.create({
+            name,
+            done,
+            projectid: projectId
+        }, {
+            fields: ['name', 'done', 'projectid']
+        });
+        res.json({
+            message: 'New task created'
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getTask(req, res) {
+    const tasks = await Task.findAll({
+        attributes: ['id', 'projectid', 'name', 'done'], //order fields
+        order: [
+            ['id', 'DESC']
+        ]
+    });
+    res.json({
+        message: 'Query success',
+        data: tasks
+    })
+
+}
+
+export async function updateTask(req, res) {
+
+}
+
+export async function deleteTask(req, res) {
+    const { id } = req.params;
+    const nRowDeleted = await Task.destroy({
+        where: {
+            id
+        }
+    });
+    res.json({
+        message: 'Task deleted',
+        data: {
+            'rowtsDeleted': nRowDeleted
+        }
+    });
+
+}
+
+export async function getTaskById(req, res) {
+
+}
+
+export async function getTaskByProject(req, res) {
+
+}
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,6 +1,11 @@
 import { Router } from 'express';
 const router = Router();
 
+import { createTask, getTask, deleteTask } from '../controllers/tastk.controller';
 
+// /api/task
+router.post('/', createTask);
+router.get('/', getTask);
+router.delete('/:id', deleteTask);
 
 export default router;
